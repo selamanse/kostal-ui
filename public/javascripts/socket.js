dbf060903c133bebf85136b0e6708c883b629705
Severin Puschkarski <severin@puschkarski.org>
2013-10-01 13:22:07 +0200

    Highlight on every data

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -16,14 +16,32 @@ socket.on('live', function (data) {
   if (data.filename ===  $('#monitor').html() && data.line){
     Kostal.update(data.line);
   }
-  $('[jq="'+data.filename+'"]').addClass('live');
+  $('[jq="'+data.filename+'"]').highlight(15000);
 });
 
 
 $('[jq]').click(function(e){
   e.preventDefault();
   e.stopPropagation();
-  jq = this.getAttribute('jq');
+  var jq = this.getAttribute('jq');
   if (jq) socket.emit('status', jq);
 });
+});
+
+$(document).ready(function () {
+
+    $.fn.highlight = function (duration, highlightColor) {
+        var highlightBg = highlightColor || "#EEEE44";
+        var animateMs = duration || "fast"; // edit is here
+        var originalBg = this.css("background-color");
+
+        if (!originalBg || originalBg === highlightBg)
+            originalBg = "#FFFFFF"; // default to white
+
+        jQuery(this)
+            .css("backgroundColor", highlightBg)
+            .animate({ backgroundColor: originalBg }, animateMs, null, function () {
+                jQuery(this).css("backgroundColor", originalBg); 
+            });
+    };
 });
